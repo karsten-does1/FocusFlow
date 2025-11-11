203aadc28199bfd7816bbc2443666a39ce9eecec
karsten-does1 <147826609+karsten-does1@users.noreply.github.com>
2025-11-11 17:55:46 +0100

    project structuur

---
+++ FocusFlow.App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FocusFlow.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
